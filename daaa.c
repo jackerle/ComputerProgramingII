0a5f6d7e52cd1c495b42145686c37a21ae32e47e
jackerle <38004405+jackerle@users.noreply.github.com>
2018-12-10 19:47:28 +0700

    Add files via upload

---
+++ daaa.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+	int number,i;
+	scanf("%d",&number);
+	for(i=0;i<number;i++){
+		printf("********* ");		
+	}printf("\n");
+	for(i=0;i<number;i++)printf("*         ");
+	printf("\n");
+	for(i=0;i<number;i++)printf("*         ");
+	printf("\n");
+	for(i=0;i<number;i++){
+		printf("********* ");		
+	}printf("\n");
+	for(i=0;i<number;i++)printf("        * ");
+	printf("\n");
+	for(i=0;i<number;i++)printf("        * ");
+	printf("\n");
+	for(i=0;i<number;i++){
+		printf("********* ");		
+	}printf("\n");
+}
