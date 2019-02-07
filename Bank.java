0281f90c4ac656855ffe9223c19641b37aa4ba67
jackerle <zeustololisis@gmail.com>
2019-02-07 15:17:32 +0700

    add lab3

--- Bank.java
+++ Bank.java
@@ -1,3 +1,4 @@
+import java.util.*;
 class Customer{
     private String name;
     private String surname;
@@ -17,6 +18,7 @@ class Customer{
         this.name=n;
         this.surname=sn;
         this.principle=principle;
+        computeInterest();
     }
     void plusMoney(){
         //???
@@ -28,13 +30,67 @@ class Customer{
         this.principle = amount;
         this.year++;
     }
+String getName(){
+        return this.name;
+    }
+    void setPrin(float p){
+        this.principle = p;
+        System.out.println("Now principle is "+p);
+    }
+
+    public float getPrinciple() {
+        return principle;
+    }
+
+    public float getInterest() {
+        return interest;
+    }
+    
+}
+class DB{
+    ArrayList<Customer> DBLis = new ArrayList<Customer>();
+    void add(String n,String sn,int principle){
+        DBLis.add(new Customer(n,sn,principle));
+    }
+    void delete(String name){
+        for(int i=0;i<DBLis.size();i++){
+            if(DBLis.get(i).getName().equals(name)){
+                DBLis.remove(i);
+                System.out.println("Now number of customer is "+DBLis.size());
+            }
+        }
+    }
+    void editPrinciple(String name,float p){
+        for(int i=0;i<DBLis.size();i++){
+            if(DBLis.get(i).getName().equals(name)){
+                DBLis.get(i).setPrin(p);
+            }
+        }
+    }
+    void totalDepos(){
+        float sum=0;
+        for(int i=0;i<DBLis.size();i++){
+            sum+=DBLis.get(i).getPrinciple();
+        }
+        System.out.println("Total is "+sum);
+    }
+    void show(){
+        for(int i=0;i<DBLis.size();i++){
+            System.out.println("Name "+DBLis.get(i).getName()+" เงินทั้งหมด "+DBLis.get(i).getPrinciple()+" ดอกเบี้ยทั้งปี "+DBLis.get(i).getInterest());
+        }
+    }
 }
 public class Bank{
     public static void main(String args[]){
-        Customer cust1 = new Customer("นายเอฟ","เรียนไม่จบ",1000);
-        Customer cust2 = new Customer("นายเอ","ก็เรียนไม่จบ",1000);
-        Customer cust3 = new Customer("นายบี","ก็คงเรียนไม่จบ",1000);
-        cust1.computeInterest();
+        DB db = new DB();
+        db.add("นายเอฟ", "เรียนไม่จบ", 1000);
+        db.add("นายเอ","เรียนไม่จบ",300000);
+        db.add("นายบี", "ก็ยังเรียนไม่จบ", 1000000);
+        db.show();
+        System.out.println("----------------deleted test---------------");
+        db.delete("นายเอฟ");
+        db.show();
+        System.out.println("--------------------------------------------");
 
     }
 
