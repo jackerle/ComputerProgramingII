574330cbade0896578a335dc794decaf70c9795f
jackerle <zeustololisis@gmail.com>
2019-03-14 13:59:04 +0700

    little train com organiz

---
+++ comp_Organiz/loopCount10.asm
@@ -0,0 +1,17 @@
+	.ORIG	x3100
+	LEA	R1,#100		
+	AND	R5,R5,#0	;initialize R5 to 0
+	AND	R6,R6,#0	;initialize R6 to 0
+	LD	R6,VALUES	; set counter
+;------------init values for loop--------------
+	LDR	R2,R1,#0	;load value from memory to R2 form 
+address R1
+	ADD	R1,R1,#1	;address R1++
+	ADD	R5,R5,R2	;R5+=R2
+	ADD	R6,R6,#-1	;counter R6--
+	BRp	#-5		;check condition if nzp = p back to LDR 
+before
+;--------------End loop-----------------
+VALUES	.FILL	#10
+	.END
+	
