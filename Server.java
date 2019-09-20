0505f05f7aea26bf3502a0942a98995d73237d60
jackerle <38004405+jackerle@users.noreply.github.com>
2019-09-21 02:28:15 +0700

    Add files via upload

--- Server.java
+++ Server.java
@@ -2,13 +2,12 @@ import java.io.*;
 import java.net.*;
 
 public class Server{
-    public final static int PORT = 6889;    //port for this socket
-    public static String PATH = "d:/4Study/OSClass/Send/";  //file to download
+    public final static int PORT = 6880;    //port for this socket
+    public static String PATH = "f:/code/OSClass/Server/";
+    public static String dir = "f:/code/OSClass/Server";
+      //file to download
 
     public static void main(String [] args) throws IOException{
-        if(args.length<1)return;
-        String file_name = args[0];
-        PATH+=file_name;
         ServerSocket server_socket = null;
         Socket socket = null;
         try {
@@ -16,10 +15,10 @@ public class Server{
             server_socket = new ServerSocket(PORT);
             while (true) {
                 
-                System.out.println("Wait...");
+                System.out.println("Server has openned on port :"+PORT);
                 socket = server_socket.accept();
                 System.out.println("Client has connected! :"+socket);
-                Thread t = new RequestHandlerThread(PATH,file_name,socket);
+                Thread t = new RequestHandlerThread(dir,PATH,socket);
                 t.start();
             }
         
@@ -36,24 +35,47 @@ class RequestHandlerThread extends Thread{
         String file_name;
         Socket socket = null;
         String path;
+        String dir;
+        DataInputStream serverData = null;
+        InputStream is = null;
 
-        public RequestHandlerThread(String Path,String file_name,Socket socket){
+        public RequestHandlerThread(String dir,String Path,Socket socket){
+            this.dir = dir;
             this.path = Path;
             this.socket = socket;
-            this.file_name = file_name;
+            //this.file_name = file_name;
         }
         public void run(){
             try{
-            File myFile = new File(path);
+            os = socket.getOutputStream();
+            is = socket.getInputStream();
+            DataOutputStream dos = new DataOutputStream(os); 
+            serverData = new DataInputStream(is);
+            File directory = new File(dir);
+            File[] flist = directory.listFiles();
+            String _list="";
+            for(File a:flist){
+                _list=_list+a.getName()+" \n";
+            }
+            dos.writeUTF(_list);
+            String file_download = serverData.readUTF();
+           System.out.println("Have a new download request file : "+file_download);
+            
+            //File myFile = new File(path);
+            File myFile = null;
+            for(File a:flist){
+                if(file_download.equals(a.getName())){
+                    myFile = a;
+                }
+            }
             byte [] byteArr = new byte[(int)myFile.length()];
             fis = new FileInputStream(myFile);
             bis = new BufferedInputStream(fis);
             bis.read(byteArr,0,byteArr.length);
-            os = socket.getOutputStream();
-            DataOutputStream dos = new DataOutputStream(os); 
-            dos.writeUTF(file_name);
+            dos.writeUTF(file_download);
             dos.writeLong(byteArr.length); 
-            System.out.println("Sending "+file_name+"("+byteArr.length +"bytes");
+           // dos.writeUTF("Hello you can do it!");
+            System.out.println("Sending "+file_download+"("+byteArr.length +"bytes");
             os.write(byteArr,0,byteArr.length);
             os.flush();
             dos.flush();
